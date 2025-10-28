67e6732e37062ce637b99ecd24a0974cd85ce106
chenry22 <112424609+chenry22@users.noreply.github.com>
2025-10-28 18:02:17 -0500

    add page flipping, fill out some of scene 1

---
+++ class-homepage/proj3/sections/pageTurner.js
@@ -0,0 +1,51 @@
+function updatePageNumURL(i) {
+    var url = new URL(window.location);
+    (url.searchParams.has('page') ? url.searchParams.set('page', i) : url.searchParams.append('page', i));
+    url.search = url.searchParams;
+    history.pushState({}, null, url.toString());
+}
+
+function previousPage() {
+    var pages = document.getElementsByClassName("page");
+    for(var i = 0; i < pages.length; i++) {
+        if (pages[i].classList.contains('active')) {
+            if (i > 0) {
+                pages[i].classList.remove('active');
+                pages[i - 1].classList.add('active');
+                updatePageNumURL(i - 1);
+            }
+            return;
+        }
+    }
+}
+
+function nextPage() {
+    var pages = document.getElementsByClassName("page");
+    for(var i = 0; i < pages.length; i++) {
+        if (pages[i].classList.contains('active')) {
+            if (i < pages.length - 1) {
+                pages[i].classList.remove('active');
+                pages[i + 1].classList.add('active');
+                updatePageNumURL(i + 1);
+            }
+            return;
+        }
+    }
+}
+
+window.addEventListener('load', (e) => {
+    document.getElementById("prev-page").addEventListener('click', (e) => {
+        previousPage();
+    }); 
+    document.getElementById("next-page").addEventListener('click', (e) => {
+        nextPage();
+    }); 
+
+    let params = new URLSearchParams(document.location.search);
+    let pageNum = parseInt(params.get("page") ?? 0);
+    let pages = document.getElementsByClassName("page");
+    if (pageNum > 0 && pageNum < pages.length) {
+        pages[0].classList.remove('active');
+        pages[pageNum].classList.add('active');
+    }
+});
